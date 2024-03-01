51c04b7d55a00ff8168867dd57b0558976b40638
supershuaifeng <limchinfeng@gmail.com>
2024-03-01 22:29:59 +0800

    output route data

--- lib/time-app/pages/home.dart
+++ lib/time-app/pages/home.dart
@@ -18,16 +18,39 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            TextButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              }, 
-              icon: Icon(Icons.edit_location), 
-              label: Text('Edit Location'),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          child: Column(
+            children: [
+              TextButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                }, 
+                icon: Icon(Icons.edit_location), 
+                label: Text('Edit Location'),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      fontSize: 28,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text(
+                data['time'],
+                style: TextStyle(
+                  fontSize: 66
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/time-app/pages/loading.dart
+++ lib/time-app/pages/loading.dart
@@ -15,7 +15,7 @@ class _LoadingState extends State<Loading> {
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    Navigator.pushReplacementNamed(context, 'home', arguments: {
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location': instance.location,
       'flag': instance.flag,
       'time': instance.time,
