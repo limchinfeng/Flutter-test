ea18a93d908dbd1780a84411f06635c558b5d652
supershuaifeng <limchinfeng@gmail.com>
2024-03-01 22:48:40 +0800

    loading animation

--- lib/time-app/pages/loading.dart
+++ lib/time-app/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services//world-time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -31,10 +32,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child: Text('loading'),
-      ),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      )
     );
   }
 }
